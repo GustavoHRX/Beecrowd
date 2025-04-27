8ed6954db8072323a930f44f0b6d2da3ac4d9683
GustavoHRX <gustavohrx123@gmail.com>
2025-04-27 19:22:27 -0300

    adição de exercicios resolvidos

---
+++ Python/1005.py
@@ -0,0 +1,9 @@
+A = float(input())
+B = float(input())
+
+res = ((A*3.5)+(B*7.5)) / 11
+
+print(f'MEDIA = {res:.5f}')
+
+''' média ponderada é o valor do produto da nota vezes o peso dividido pela soma dos pesos 
+Sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11)'''
